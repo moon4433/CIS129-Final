566c6506000a89b8e4d880dd2387ddd6358d14a8
Keegan <johnk117@ferris.edu>
2018-12-12 00:21:35 -0500

    Started on homework

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace JohnsonK_Final
 {
     class Program
     {
+        Player currentPlayer;
+        Card deck = new Card();
+
         static void Main(string[] args)
         {
         }
@@ -16,14 +19,60 @@ namespace JohnsonK_Final
     class Player
     {
         ArrayList matchPile = new ArrayList();
+        static ArrayList playerHand = new ArrayList();
+        public bool isPickingCard = false;
+        Card firstCard;
+        Card secondCard;
 
         public Player()
         {
+            checkForMatches();
+        }
+        public void checkForMatches()
+        {
+            foreach (Card firstCard in playerHand)
+            {
+                foreach(Card secondCard in playerHand)
+                {
+                    if(firstCard.cardNum == secondCard.cardNum)
+                    {
+                        if(firstCard.cardAmount == 1 && secondCard.cardAmount == 2)
+                        {
 
+                        }
+                    }
+                }
+            }
         }
     }
-    class Deck
+    class Card
     {
+        public int cardNum;
+        public string name;
+        public int cardAmount;
+        private int greenThumbGertAmount = 0;
+        private int arnieAnglerAmount = 0;
+        private int haySeedHankAmount = 0;
+        private int milkManMoAmount = 0;
+        private int lassoLouieAmount = 0;
+        private int freddieFallOffAmount = 0;
+        private int carelessCarrieAmount = 0;
+        private int fiFiFluffAmount = 0;
+        private int altoAnnieAmount = 0;
+        private int balletBettyAmount = 0;
+        private int oldMaidAmount = 0;
+        public bool isGreenThumbGertOut = false;
+        public bool isArnieAnglerOut = false;
+        public bool isHaySeedHankOut = false;
+        public bool isMilkManMoOut = false;
+        public bool isOldMaidOut = false;
+        public bool isLassoLouieOut = false;
+        public bool isFreddieFallOffOut = false;
+        public bool isCarelessCarrieOut = false;
+        public bool isFiFiFluffOut = false;
+        public bool isAltoAnnieOut = false;
+        public bool isBalletBettyOut = false;
+
         public static void DealDeck()
         {
 
