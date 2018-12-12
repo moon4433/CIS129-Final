d2df2ff47b69631a988b764c19fc07fef44fbaf6
Keegan <42782601+moon4433@users.noreply.github.com>
2018-12-11 22:57:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohnsonK_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Player
+    {
+        ArrayList matchPile = new ArrayList();
+
+        public Player()
+        {
+
+        }
+    }
+    class Deck
+    {
+        public static void DealDeck()
+        {
+
+        }
+    }
+}
